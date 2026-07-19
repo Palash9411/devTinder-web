01967c63baef9053a265f5a9b76f5003ea311523
Palash <palashgupta9411@gmail.com>
2026-07-19 15:36:11 +0530

    logout integration

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,26 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { deleteUser } from '../utils/userSlice';
+import { BASE_URL } from "../utils/contants";
 const NavBar = () => {
   const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  const handleLogout = async () => {
+    try {
+       const response = await axios.post(`${BASE_URL}logout`, {}, { withCredentials: true });
+       if (response.status === 200) {
+          dispatch(deleteUser());
+         return navigate("/login");
+       } else {
+          console.error("Logout failed:", response.statusText);
+       }
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
@@ -41,7 +58,7 @@ const NavBar = () => {
                 <Link to="/settings">Settings</Link>
               </li>
               <li>
-                <Link to="/logout">Logout</Link>
+                <Link onClick={handleLogout} to="/logout">Logout</Link>
               </li>
             </ul>
           </div>
